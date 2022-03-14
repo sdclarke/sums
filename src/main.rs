f93346e847e7fbf0ad0885e9cb21e3f88ce64aaa
Scott Clarke <scott.clarke@codethink.co.uk>
2022-03-14 14:26:06 +0000

    Use BufReader instead of `read_to_string`
    This avoids the need to have the contents of file loaded into memory in
    its enitrety at one time.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,41 @@
 use std::env::args;
 use std::error::Error;
-use std::fs;
-use std::num::ParseIntError;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Error as IoError, ErrorKind};
+//use std::num::ParseIntError;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Get the filename which will be the second element of the args
     let filename = args().nth(1).ok_or("Expected filename")?;
-    // Read the contents of the file to a string
-    // Give an nice error message if it fails
-    let mut contents = fs::read_to_string(filename.clone()).or(Err(format!(
-        "Could not read from the file `{}`, does it exist with the correct permissions and contain valid UTF-8?",
-        filename
-    )))?;
-    // Remove any trailing whitespace
-    contents = contents.trim_end().to_string();
-    // Check for an empty file
-    if contents.is_empty() {
-        return Err("The file was empty or contained only whitespace".into());
+    // Open the file
+    let file = File::open(filename.clone())?;
+    // Fail early if the file is empty
+    if file.metadata()?.len() == 0 {
+        return Err("File is empty".into());
     }
-    let sum: i64 = contents
-        .split('\n') // Split into lines
-        .map(|e| e.parse::<i64>()) // Parse each line as i64
-        .collect::<Result<Vec<i64>, ParseIntError>>() // Collect iterator as a `Result` containing a Vec<i64> or and Error
-        .or(Err("Encountered something which was not an integer"))? // Check the error from parsing
-        .iter() // Turn Vec to Iterator
-        .sum(); // Sum up elements of the iterator
+    // Wrap the file in a BufReader
+    let file = BufReader::new(file);
+    let sum: i64 = file
+        .lines() // Get the lines of the file
+        .map(|l| {
+            // Parse each line as an i64
+            l.and_then(|v| {
+                v.parse::<i64>()
+                    // Map the error to std::io::Error to make `and_then` work
+                    .map_err(|e| IoError::new(ErrorKind::InvalidData, e))
+            })
+        })
+        .collect::<Result<Vec<i64>, IoError>>() // Collect the iterator as a Result containing the Vec<i64>
+        .map_err(|e| {
+            // Turn the error into something nicer to output
+            format!(
+                "There was a problem parsing the numbers in the file `{}`: {}",
+                filename,
+                e.to_string()
+            )
+        })?
+        .iter() // Convert to iterator and sum
+        .sum();
     println!("The sum is {:?}", sum);
     Ok(())
 }
